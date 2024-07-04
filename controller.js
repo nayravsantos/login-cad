8febb3460f14492d931846d93e4178a03b77b379
Nayra Vitória dos Santos <santos.nayra@alunoescolassociais.org.br>
2024-07-04 11:30:01 -0300

    Validação de acesso.

---
+++ controller.js
@@ -0,0 +1,14 @@
+//FUNÇÃO PARA VALIDAÇÃO DE ACESSO  
+ 
+function acessar(){
+    let loginEmail =document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+ 
+    if(!loginEmail || !loginSenha){
+        alert("Favor preencher todos os campos");
+    }else{
+        //alert("Campos preenchidos com sucesso");
+        window.location.href = 'cadastro.html';
+    }
+}
+ 
