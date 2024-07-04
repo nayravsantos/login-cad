26e6a71c23e95ad6df40c490068e8d280bb6c6b6
Nayra Vitória dos Santos <santos.nayra@alunoescolassociais.org.br>
2024-07-04 11:59:06 -0300

    Informar nome para cadastro.

--- controller.js
+++ controller.js
@@ -15,5 +15,12 @@ function acessar(){
 // FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
 var dadoLista = [];
 function salvarUser(){
-   
+    let nomeUser = document.getElementById
+    ('nomeUser').value;
+
+    if(nomeUser){
+
+    }else{
+        alert("Favor informe um nome para cadastro");
+    }
 }
