8a7d17e332561d3a302c9e57bc2ebe7919d27487
Nayra Vitória dos Santos <santos.nayra@alunoescolassociais.org.br>
2024-08-09 08:27:28 -0300

    Adicionando comentários.

--- controller.js
+++ controller.js
@@ -3,7 +3,8 @@
 function acessar(){
     let loginEmail =document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
- 
+    
+//
     if(!loginEmail || !loginSenha){
         alert("Favor preencher todos os campos");
     }else{
@@ -19,8 +20,39 @@ function salvarUser(){
     ('nomeUser').value;
 
     if(nomeUser){
+        dadoLista.push(nomeUser);
+        //console.log(dadosLista);
+        criaLista();
 
+// LIMPAR O TEXTO NO CAMPO DE ENTRADA
+        document.getElementById('nomeUser').value = "";
     }else{
-        alert("Favor informe um nome para cadastro");
+        alert("Por favor informe um nome para cadastro");
     }
+}
+
+// FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
+function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th></th>Ações</th</tr>";
+
+// ITERA SOBRE CADA ELEMENTO DO ARRAY 'DADOSLISTA'
+    for(let i=0;i <= (dadosLista.length - 1);i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type= 'button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type= 'button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+
+  // ESSA LINHA DEFINE O HTML INTERNO DO ELEMENTO
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+
+}
+
+// FUNÇÃO PARA EDITAR NOMES DE LISTA
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadoLista.splice(dadoLista[(i - 1)], 1);
+}
+
+// FUNÇÃO PARA EXCLUIR NOME DA LISTA
+function excluir(i){
+    dadoLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
 }
